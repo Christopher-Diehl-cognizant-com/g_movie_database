c37410d093be91bce69da08e6815efc3954e4d55
Vinay <vinay.py@gmail.com>
2021-03-20 12:45:48 -0400

    Acceptance 2 - GREEN Test - Given a new movie has released                           When I submit this new movie to GMDB movies                           Then I should see that movie in GMDB movies

--- src/main/java/com/galvanize/gmoviedatabase/GMovieDatabaseController.java
+++ src/main/java/com/galvanize/gmoviedatabase/GMovieDatabaseController.java
@@ -4,23 +4,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 @RestController
 @RequestMapping("/movies")
 public class GMovieDatabaseController {
-    Movie movie;
+    List<Movie> movies = new ArrayList<>();
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Movie> getMovies(){
-        return Collections.emptyList();
+        return this.movies;
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public void postMovies(@RequestBody Movie movie){
-        this.movie = movie;
+        this.movies.add(movie);
     }
 
 }
--- src/test/java/com/galvanize/gmoviedatabase/GMovieDatabaseApplicationTests.java
+++ src/test/java/com/galvanize/gmoviedatabase/GMovieDatabaseApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -20,6 +21,7 @@ class GMovieDatabaseApplicationTests {
 	ObjectMapper objectMapper;
 
 	@Test
+	@DirtiesContext
 	public void emptyDBTest() throws Exception {
 			this.mockMvc.perform(MockMvcRequestBuilders.get("/movies")
 					.contentType(MediaType.APPLICATION_JSON))
@@ -28,6 +30,7 @@ class GMovieDatabaseApplicationTests {
 	}
 
 	@Test
+	@DirtiesContext
 	public void newMovieReleasedTest() throws Exception{
 
 
