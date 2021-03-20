eace127a861ef352203e73a41b8bd5098ab54b37
cld71 <cld71@cld-pc2.diehl>
2021-03-20 09:59:38 -0500

    first commit

---
+++ src/main/java/com/galvanize/gmoviedatabase/GmoviedatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.gmoviedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmoviedatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmoviedatabaseApplication.class, args);
+	}
+
+}
